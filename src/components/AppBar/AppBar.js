2657679be65a184707fdb8d916bfee4121811794
ShrutikaSingh <shrutika051220@gmail.com>
2023-09-23 20:21:52 -0700

    upate app bar with menus and mui

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+// import Button from '@mui/material/Button';
+// import Actions from './Actions'; // You should import Actions and ManageAccount components.
+// import ManageAccount from './ManageAccount';
 
-const useStyles = makeStyles((theme) => ({
+const theme = createTheme();
+
+const useStyles = {
   root: {
     flexGrow: 1,
   },
@@ -16,24 +21,49 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-}));
+};
 
-function Appbar() {
-  const classes = useStyles();
+function Appbar({ todos, isLoggedIn, onLogin, onLogout }) {
+  const [isAccountModalOpen, setAccountModalOpen] = useState(false);
+
+  const handleLogoutClick = () => {
+    onLogout(); // Call the parent component's logout function
+  };
+
+  const handleAccountManagement = () => {
+    setAccountModalOpen(true);
+  };
+
+  const handleDeleteAccount = () => {
+    // Clear local storage and log out
+    localStorage.clear();
+    handleLogoutClick();
+    setAccountModalOpen(false);
+  };
+
+  const username = localStorage.getItem('username');
 
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Grocery App
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={useStyles.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={useStyles.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={useStyles.title}>
+              Grocery App
+            </Typography>
+
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
 }
 
