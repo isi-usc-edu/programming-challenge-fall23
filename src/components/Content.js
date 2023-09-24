00f355a983bac087b58dd4025398193dd92b783f
ShrutikaSingh <shrutika051220@gmail.com>
2023-09-23 18:12:24 -0700

    add appBar at the top

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import Typography from '@mui/material/Typography'
-
+import Appbar from './AppBar/AppBar';
 
 const Content = ({ socketConnection }) => {
 
   const renderHello = () => {
-    return <Typography variant="h1">hello!</Typography>
+
+    return (
+      <>
+        <AppBar ></AppBar>
+    <Typography variant="h1">hello!</Typography>
+    </>
+    )
   }
 
   return (
